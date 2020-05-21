83cd279a3731c2a6ba6b7a1ab0a36286360473a5
naga279 <gana.profile27@gmail.com>
2020-05-21 17:25:33 +0530

    Publisher Api has started

--- src/main/java/com/naga/course/apis/libraryapis/LibraryApisApplication.java
+++ src/main/java/com/naga/course/apis/libraryapis/LibraryApisApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.naga.course.apis.libraryapis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
